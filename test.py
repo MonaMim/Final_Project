95615004a9e16188555b3ed309f2450ca1738ef3
MonaMim <mona_2424@yahoo.com>
2018-08-07 23:31:59 +0430

    Update test.py

--- test.py
+++ test.py
@@ -22,14 +22,26 @@ class AudioProccessor:
             i = i + 1
         return listOfNewAudios
 
-    def wav2mfcc(source_path, max_pad_len=11):
-        fixpath = os.path.join(source_path, "splitted1.wav")
-        wave, sr = librosa.load(fixpath, mono=True, sr=None)
-        wave = wave[::3]
-        mfcc = librosa.feature.mfcc(wave, sr=16000)
-        pad_width = max_pad_len - mfcc.shape[1]
-        mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
-        return mfcc
+#    def wav2mfcc(self, source_path, max_pad_len=11):
+#        fixpath = os.path.join(source_path, "splitted1.wav")
+        #wave, sr = librosa.load(fixpath, mono=True, sr=None)
+    def wav2mfcc(this, file_path, max_len=11): #def wav2mfcc(file_path, max_len=11):
+            wave, sr = librosa.load(os.path.abspath(file_path), mono=True, sr=None)
+            print(wave.shape)
+            print(wave)
+            wave = wave[::3]
+            mfcc = librosa.feature.mfcc(wave, sr=16000)
+
+            # If maximum length exceeds mfcc lengths then pad the remaining ones
+            if (max_len > mfcc.shape[1]):
+                pad_width = max_len - mfcc.shape[1]
+                mfcc = np.pad(mfcc, pad_width=((0, 0), (0, pad_width)), mode='constant')
+
+            # Else cutoff the remaining parts
+            else:
+                mfcc = mfcc[:, :max_len]
+            
+            return mfcc
 
     # Input: Folder Path
     # Output: Tuple (Label, Indices of the labels)
@@ -53,8 +65,8 @@ class AudioProccessor:
             np.vstack((mfcc_vectors, mfcc_vector))
         return mfcc_vectors
 
-#AudioProccessor().createDirectory('CHAHARGAH')
-#AudioProccessor().mp3Split('Shadjarian_Overture_CHAHARGAH.mp3', 'CHAHARGAH')
+AudioProccessor().createDirectory('CHAHARGAH')
+AudioProccessor().mp3Split('Shadjarian_Overture_CHAHARGAH.mp3', 'CHAHARGAH')
 #data = AudioProccessor().save_data_to_array('CHAHARGAH')
-data = AudioProccessor().wav2mfcc('/CHAHARGAH/')
+data = AudioProccessor().wav2mfcc('CHAHARGAH/splitted1.wav')
 #print(data)
