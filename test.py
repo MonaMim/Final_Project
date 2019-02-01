551a26a3b0ec7850d46a572c8b11fc230abe6168
MonaMim <mona_2424@yahoo.com>
2019-02-01 09:44:04 +0330

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,15 @@ import numpy as np
 import os
 from pydub import AudioSegment
 import librosa
-from tqdm import tqdm
+from sklearn.model_selection import train_test_split
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Conv2D, MaxPooling2D
+from keras import backend as K
+
 
 class AudioProccessor:
+
+    dastgah_to_index = {"NAVA":0, "MAHOUR":1, "SHOUR":2, "CHAHARGAH":3, "HOMAYOUN":4, "SEGAH":5, "RASTEPANJGAH":6}
     np.set_printoptions(threshold=np.nan)
     counter = 1
 
@@ -28,7 +34,7 @@ class AudioProccessor:
             AudioProccessor.counter = AudioProccessor.counter + 1
         return listOfNewAudios
 
-    def wav2mfcc(this, file_path, max_len=1): 
+    def wav2mfcc(this, file_path, max_len): 
         #sr = sample rate
         wave, sr = librosa.load(os.path.abspath(file_path), mono=True, sr=None)
         #print(wave.shape)
@@ -44,10 +50,13 @@ class AudioProccessor:
         # Else cutoff the remaining parts
         else:
             mfcc = mfcc[:, :max_len]
+            #print (mfcc.shape)
         
         return mfcc
 
-    def save_data_to_array(this, path, max_len=1):
+
+
+    def save_data_to_array(this, path, max_len=11):
 
         mfcc_vectors = []
         mfcc_vector_main = []
@@ -74,52 +83,6 @@ class AudioProccessor:
         for wavfile in wavfiles:
             AudioProccessor().mp3Split(wavfile, DASTGAH_destination_path)
 
-    def to_categorical(this, vector_y): #lookup table
-        categorical_vec = [0,0,0,0,0,0,0]
-        y2 = np.empty([len(vector_y), len(categorical_vec)])
-        #categorical_vec = np.empty([len(vector_y),7], dtype=int)
-        #["Nava", "Mahour", "Shour", "Chahargah", "Homayoun", "Segah", "Rastgepanjgah"]
-        #y = np.empty(len(vector_y))
-
-        for x , a in enumerate(vector_y):
-                    
-            categorical_vec = [0,0,0,0,0,0,0]    
-            if vector_y[x]=="NAVA":
-                categorical_vec[0]=1
-            if vector_y[x]=="MAHOUR":
-                categorical_vec[1]=1
-            if vector_y[x]=="SHOUR":
-                categorical_vec[2]=1
-            if vector_y[x]=="CHAHARGAH":
-                categorical_vec[3]=1
-            if vector_y[x]=="HOMAYOUN":
-                categorical_vec[4]=1
-            if vector_y[x]=="SEGAH":
-                categorical_vec[5]=1
-            if vector_y[x]=="RASTEPANJGAH":
-                categorical_vec[6]=1
-            
-            y2  = np.vstack((y2, categorical_vec))
-            #categorical_vec = [0,0,0,0,0,0,0]
-        #print y
-        return y2
-
-    def categorical(this, vector_y):
-
-        y2 = np.empty([len(vector_y), 7])
-
-        for x , a in enumerate(vector_y):
-            switcher={
-                   'NAVA': [1,0,0,0,0,0,0],
-                   'MAHOUR':[0,1,0,0,0,0,0],
-                   'SHOUR':[0,0,1,0,0,0,0],
-                   'CHAHARGAH':[0,0,0,1,0,0,0],
-                   'HOMAYOUN':[0,0,0,0,1,0,0],
-                   'SEGAH':[0,0,0,0,0,1,0],
-                   'RASTEPANJGAH':[0,0,0,0,0,0,1]
-                 }
-            y2  = np.vstack((y2, switcher.get(vector_y[x],"Invalid Input")))
-        return y2
 
     def set_X_Y(this, DASTGAH):
         AudioProccessor().split_group(DASTGAH+"_Raw", DASTGAH+ "_Splitted")
@@ -127,13 +90,67 @@ class AudioProccessor:
         DASTGAH_Y = len(DASTGAH_X) *[DASTGAH]
         return DASTGAH_X, DASTGAH_Y
 
+    def to_categorical(this, vector_y): #lookup table
+        y2 = np.zeros([len(vector_y), 7])
+        for x , a in enumerate(vector_y):
+            y2[x, AudioProccessor.dastgah_to_index[a]] = 1 
+        return y2
+
+    def get_train_test(this, split_ratio, random_state, X, Y):
+        return train_test_split(X, Y, test_size= (1 - split_ratio), random_state=random_state, shuffle=True)
+
+
+
+#mfcc = AudioProccessor().wav2mfcc('Ahmad_Ebadi_CHAHARGAH.mp3')
+#print (mfcc.shape)
+
 
 Chahargah_X, Chahargah_Y = AudioProccessor().set_X_Y("CHAHARGAH")
 Nava_X, Nava_Y = AudioProccessor().set_X_Y("NAVA")
 
-Y = Chahargah_Y + Nava_Y 
+#Y = Chahargah_Y + Nava_Y 
+Y = np.append(Chahargah_Y, Nava_Y)
+#print len(Nava_Y), len(Chahargah_Y), len(Y) 
 X = np.vstack((Chahargah_X, Nava_X))
 Y = AudioProccessor().to_categorical(Y)
-#Y = AudioProccessor().categorical(Y)
 
-print Y
+#assert X.shape[0] == len(Y)
+
+X_train, X_test, Y_train, Y_test = AudioProccessor().get_train_test(0.6, 42, X, Y)
+X_train = X_train.reshape(X_train.shape[0], 20, 11, 1)
+X_test = X_test.reshape(X_test.shape[0], 20, 11, 1)
+
+print (X_train.shape)
+print (X_train.size)
+'''
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(1,2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+
+#mfcc = AudioProccessor().wav2mfcc("CHAHARGAH_Raw/Ahmad_Ebadi_CHAHARGAH.mp3", max_len=11) 
+#print (mfcc)
+#print (X_test.shape)
+#print (X_train.shape)
+
+    # Append all of the dataset into one single array, same goes for y
+
+'''
